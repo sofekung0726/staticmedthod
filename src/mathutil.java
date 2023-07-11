ea899954c32286bdec76e4b9dc9f4acf37929fbf
sofekung0726 <102461420+sofekung0726@users.noreply.github.com>
2023-07-11 22:22:47 +0700

    fix all

---
+++ src/mathutil.java
@@ -0,0 +1,12 @@
+
+public class mathutil {
+	public static int add(int a, int b)
+	{         return a + b;     }   
+	public static int subtract(int a, int b)
+	{         return a - b;     }    
+	public static int multiply(int a, int b)
+	{         return a * b;     }    
+	public static int divide(int a, int b)
+	{         return a / b;     }
+}
+
---
+++ src/testdrive.java
@@ -0,0 +1,6 @@
+
+public class testdrive {
+	public static void main(String[] args) {
+		System.out.println(mathutil.add(5,45));
+	}
+}
